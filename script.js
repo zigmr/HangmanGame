e04c464945fcc56a1d025eb0009eefc43e79e91d
Aliyah Lewis <aliyah.y.lewis@gmail.com>
2023-11-29 13:23:32 -0500

    Updates

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function redirectBtn() {
 
     window.location.href = URL;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate loading time (you can replace this with actual loading logic)
+    setTimeout(function () {
+        // Hide the loading animation
+        document.querySelector('.loader-wrapper').style.display = 'none';
+
+        // Play the pop sound
+        document.getElementById('popSound').play();
+
+        // Show the content
+        document.getElementById('content').style.display = 'block';
+
+        // Play background music
+        document.getElementById('backgroundMusic').play();
+    }, 1000); // Adjust the time as needed
+});

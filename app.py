6c2c395f82612ba015e86805d84d44cf99c07642
Aliyah Lewis <aliyah.y.lewis@gmail.com>
2023-11-29 13:48:36 -0500

    Updates

--- app.py
+++ app.py
@@ -21,8 +21,23 @@ def check_guess(word, guess):
 
 # Demonstrates the use of lists (learned in data structures course)
 def get_word(difficulty):
-    easy_words = ['apple', 'banana', 'orange']
-    hard_words = ['elephant', 'giraffe', 'rhinoceros']
+    easy_words = ["apple", "banana", "cat", "dog", "elephant", "fish", "green", "happy",
+                 "island", "jacket", "kite", "lemon", "mouse", "nest", "orange", "purple",
+                 "queen", "rabbit", "sun", "turtle", "umbrella", "violet", "water", "xylophone",
+                 "yellow", "zebra", "air", "bird", "cloud", "dolphin", "egg", "flower", "guitar",
+                 "hat", "ice cream", "juice", "key", "laptop", "moon", "notebook", "ocean", "pencil",
+                 "quiet", "rose", "star", "tree", "unicorn", "volcano", "window", "xylophone", "zeppelin"]
+    hard_words = ["antidisestablishmentarianism", "benevolence", "cacophony", "deoxyribonucleic", "ephemeral",
+                 "facetious", "garrulous", "higgledy-piggledy", "idiosyncrasy", "juxtaposition",
+                 "kaleidoscopic", "labyrinthine", "mnemonic", "obfuscate", "peculiar",
+                 "quintessential", "recalcitrant", "sesquipedalian", "triskaidekaphobia", "ubiquitous",
+                 "vexatious", "weltschmerz", "xanthosis", "yurt", "zeitgeist",
+                 "aberration", "belligerent", "circumlocution", "disparate", "exacerbate",
+                 "flibbertigibbet", "grandiloquent", "hierarchical", "iconoclast", "jingoistic",
+                 "kowtow", "lugubrious", "mellifluous", "nepotism", "obfuscation",
+                 "peregrinate", "quixotic", "ratiocination", "sesquipedalian", "taciturn",
+                 "ubiquity", "verisimilitude", "welter", "xenophobe", "yammer",
+                 "zymurgy"]
 
     word_list = easy_words if difficulty == 'easy' else hard_words
     return random.choice(word_list)

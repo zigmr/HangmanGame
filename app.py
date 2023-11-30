3748695c016be5d38aeac0949405c5b57fdcc1a7
Aliyah Lewis <aliyah.y.lewis@gmail.com>
2023-11-29 19:57:24 -0500

    Updates

--- app.py
+++ app.py
@@ -3,37 +3,24 @@ import random
 
 app = Flask(__name__)
 
-word_lists = {
-    "easy": ["apple", "banana", "cat", "dog", "elephant", "fish", "green", "happy",
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/game/easy")
+def hangman():
+    return render_template("game/easy.html")
+
+@app.route("/get_word", methods=["POST"])
+def get_word():
+    words = ["apple", "banana", "cat", "dog", "elephant", "fish", "green", "happy",
                  "island", "jacket", "kite", "lemon", "mouse", "nest", "orange", "purple",
                  "queen", "rabbit", "sun", "turtle", "umbrella", "violet", "water", "xylophone",
                  "yellow", "zebra", "air", "bird", "cloud", "dolphin", "egg", "flower", "guitar",
                  "hat", "ice cream", "juice", "key", "laptop", "moon", "notebook", "ocean", "pencil",
-                 "quiet", "rose", "star", "tree", "unicorn", "volcano", "window", "xylophone", "zeppelin"],
-    "hard": ["antidisestablishmentarianism", "benevolence", "cacophony", "deoxyribonucleic", "ephemeral",
-                 "facetious", "garrulous", "higgledy-piggledy", "idiosyncrasy", "juxtaposition",
-                 "kaleidoscopic", "labyrinthine", "mnemonic", "obfuscate", "peculiar",
-                 "quintessential", "recalcitrant", "sesquipedalian", "triskaidekaphobia", "ubiquitous",
-                 "vexatious", "weltschmerz", "xanthosis", "yurt", "zeitgeist",
-                 "aberration", "belligerent", "circumlocution", "disparate", "exacerbate",
-                 "flibbertigibbet", "grandiloquent", "hierarchical", "iconoclast", "jingoistic",
-                 "kowtow", "lugubrious", "mellifluous", "nepotism", "obfuscation",
-                 "peregrinate", "quixotic", "ratiocination", "sesquipedalian", "taciturn",
-                 "ubiquity", "verisimilitude", "welter", "xenophobe", "yammer",
-                 "zymurgy"]
-}
+                 "quiet", "rose", "star", "tree", "unicorn", "volcano", "window", "xylophone", "zeppelin"]
+    word = random.choice(words)
+    return jsonify({"word": word})
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get_word/<difficulty>', methods=['GET'])
-def get_word(difficulty):
-    word = select_word(difficulty)
-    return jsonify({'word': word})
-
-def select_word(difficulty):
-    return random.choice(word_lists[difficulty])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- game/script.js
+++ game/script.js
@@ -1,6 +1,6 @@
+// Your existing code
 function redirectBtn() {
     var URL = "play.html";
-
     window.location.href = URL;
 }
 
@@ -29,6 +29,73 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById('clickSound').play();
             });
         });
-    }, 1000); // Adjust the time as needed
-});
 
+        // Hangman game logic
+        var wordToGuess = ""; // The word to guess
+        var guessedLetters = []; // Guessed letters
+        var attemptsLeft = 6; // Number of attempts allowed
+
+        // Function to choose a random word
+        function chooseWord() {
+            var words = ["python", "hangman", "programming", "computer", "game"];
+            return words[Math.floor(Math.random() * words.length)];
+        }
+
+        // Function to display the word with underscores for unguessed letters
+        function displayWord() {
+            var display = "";
+            for (var i = 0; i < wordToGuess.length; i++) {
+                if (guessedLetters.includes(wordToGuess[i])) {
+                    display += wordToGuess[i];
+                } else {
+                    display += "_";
+                }
+            }
+            document.getElementById('word-lines').textContent = display;
+        }
+
+        // Function to check the guessed letter
+        function checkGuess(letter) {
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter);
+                if (!wordToGuess.includes(letter)) {
+                    attemptsLeft--;
+                }
+                displayWord();
+                checkGameStatus();
+            }
+        }
+
+        // Function to check the game status (win or lose)
+        function checkGameStatus() {
+            if (attemptsLeft === 0) {
+                alert('Game over! You ran out of attempts. The word was: ' + wordToGuess);
+                resetGame();
+            } else if (!displayWord().includes('_')) {
+                alert('Congratulations! You guessed the word: ' + wordToGuess);
+                resetGame();
+            }
+        }
+
+        // Function to reset the game
+        function resetGame() {
+            wordToGuess = chooseWord();
+            guessedLetters = [];
+            attemptsLeft = 6;
+            displayWord();
+        }
+
+        // Initialize the game
+        wordToGuess = chooseWord();
+        displayWord();
+
+        // Add click event listeners to letter buttons
+        letterButtons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                checkGuess(button.textContent.toLowerCase());
+            });
+        });
+
+    }, 1000); // Adjust the time as needed
+
+});
